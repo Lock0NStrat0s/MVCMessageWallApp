05ffc038f592cdce3b75c867d2d76bfebaab136b
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-29 08:54:26 -0600

    Add project files.

---
+++ MVCMessageWall/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCMessageWall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
